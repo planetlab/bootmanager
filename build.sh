e616a2459472173f45d29864cc20e2b349bf4994
Aaron Klingaman <alk@cs.princeton.edu>
2005-05-26 17:10:19 +0000

    this will have to suffice until a proper make/build system is created. this compresses the bootmanager sources, base64 encodes them, and creates a single bash script that does the reverse of that, and executes the bootmanager

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# build a bash script that can be executed by the boot cd,
+# and contains embedded in it the boot manager.
+
+DEST_SCRIPT=bootmanager.sh
+
+cat > $DEST_SCRIPT << '_EOF_'
+#!/bin/bash
+set -e
+
+UUDECODE=/usr/bin/uudecode
+
+# once we get the beta cds out of use, this can be removed
+if [ ! -x $UUDECODE ]; then
+  UUDECODE=/tmp/uudecode
+  curl -s http://boot.planet-lab.org/alpina-v3/uudecode.gz | gzip -d -c > $UUDECODE
+  chmod +x $UUDECODE
+fi
+
+_EOF_
+echo '($UUDECODE | /bin/tar -C /tmp -xj) << _EOF_' >> $DEST_SCRIPT
+tar -cj source/ | uuencode -m - >> $DEST_SCRIPT
+echo '_EOF_' >> $DEST_SCRIPT
+echo 'cd /tmp/source' >> $DEST_SCRIPT
+echo 'chmod +x BootManager.py && ./BootManager.py' >> $DEST_SCRIPT
